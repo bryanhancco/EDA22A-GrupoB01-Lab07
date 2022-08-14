3a55af6a09ecc9f838f4610b96912ca6ffb374dd
bryanhancco <bhanccoco@unsa.edu.pe>
2022-08-14 11:00:32 -0500

    Agregando clase Node

---
+++ src/Propuestos/Node.java
@@ -0,0 +1,36 @@
+public class Node<K, V> {
+  //Atributos
+  private K key;
+  private V value;
+  
+  //Constructor
+  public Nodo(K key, V value) {
+      this.key = key;
+      this.value = value;
+  }
+  
+  //Setter y Getter
+  public K getKey() {
+      return key;
+  }
+
+  public void setKey(K key) {
+      this.key = key;
+  }
+  public V getValue() {
+      return value;
+  }
+
+  public void setValue(V value) {
+      this.value = value;
+  }
+  public boolean equals(Nodo<K, V> obj) {
+      return false;
+  }
+
+  @Override
+  public String toString() {
+      return "<" + "key= " + key +", value= " + value + ">";
+  }
+
+}
