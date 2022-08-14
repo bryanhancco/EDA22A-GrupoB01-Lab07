046d5ab577dbd7835920c6a474bfa60ca1259216
fluchi <fcardenasm@unsa.edu.pe>
2022-08-14 13:15:38 -0500

    pequeñas modificaciones en interfaz

--- src/Propuestos/HashTable.java
+++ src/Propuestos/HashTable.java
@@ -1,6 +1,6 @@
-package Propuestos;
+// package Propuestos;
 
-public interface HashTable{
+public interface HashTable<K, V> {
     /**
      * Retorna el numero de clave-valor registrado en la tabla hash.
      *
@@ -16,65 +16,72 @@ public interface HashTable{
     boolean isEmpty();
 
     /**
-     * Retorna {@code true} Si la tabla contiene una clave determinada. Retorna {@code true} si y solo si 
+     * Retorna {@code true} Si la tabla contiene una clave determinada. Retorna
+     * {@code true} si y solo si
      * existe la clave {@code k} una sola vez)
      *
      * @param key la clave cuya existencia sera validada
      * @return {@code true} si la tabla contiene un la clave determinada
      * @throws NullPointerException si la clave es null
-     * (<a href="{@docRoot}/java/util/Collection.html#optional-restrictions">optional</a>)
+     *                              (<a href=
+     *                              "{@docRoot}/java/util/Collection.html#optional-restrictions">optional</a>)
      */
-    boolean containsKey(Object key);
+    boolean containsKey(K key);
 
     /**
-     * Returns {@code true} Si la tabla contiene uno o mas registros asociados a   
+     * Returns {@code true} Si la tabla contiene uno o mas registros asociados a
      * cierto valor especificado.
      *
      * @param value valor cuya existencia sera validada
-     * @return {@code true} si la tabla contiene una o mas claves asociadas al valor determinado
+     * @return {@code true} si la tabla contiene una o mas claves asociadas al valor
+     *         determinado
      * @throws NullPointerException si el valor es null
-     * (<a href="{@docRoot}/java/util/Collection.html#optional-restrictions">optional</a>)
+     *                              (<a href=
+     *                              "{@docRoot}/java/util/Collection.html#optional-restrictions">optional</a>)
      */
-    boolean containsValue(Object value);
+    boolean containsValue(V value);
 
     /**
-     * Retorna el valor el cual corresponde a la clave o 
+     * Retorna el valor el cual corresponde a la clave o
      * {@code null} si la tabla no contiene el registro para la clave.
+     * 
      * @param key la clave cuyo valor asociado sera retornado
-     * @return el valor correspondiente a la clave o 
+     * @return el valor correspondiente a la clave o
      *         {@code null} si no se encuentra un registro para la clave
      * @throws NullPointerException si la clave es null
-     * (<a href="{@docRoot}/java/util/Collection.html#optional-restrictions">optional</a>)
+     *                              (<a href=
+     *                              "{@docRoot}/java/util/Collection.html#optional-restrictions">optional</a>)
      */
-    Integer get(String key);
-
+    Integer get(K key);
 
     /**
      * Asocia el valor especificado con la clave especificada a la tabla hash
      * Si la tabla tiene un registro previo para la clave,
      * el valor viejo es reemplazado por el nuevo valor ingresado.
      *
-     * @param key la clave
+     * @param key   la clave
      * @param value el valor
      * @return El valor previo al valor asociado con la clave {@code key}, o
-     *         {@code null} si no existia previamente un reistro con la clave {@code key} y 
-	 * 			este fue agregado correctamente.
+     *         {@code null} si no existia previamente un reistro con la clave
+     *         {@code key} y
+     *         este fue agregado correctamente.
      * @throws NullPointerException si la clave o valor son nulos
-     * @throws ErrorException para otros tipos de error
+     * @throws ErrorException       para otros tipos de error
      */
-    Integer put(String key, Integer value);
+    void put(K key, V value);
 
     /**
-     * Elimina el registro de una clave, si la clave existe 
+     * Elimina el registro de una clave, si la clave existe
      * o {@code null} si no existe un registro para la clave.
      *
      * @param key clave a ser removida de la tabla hash
      * @return el valor que fue eliminado y asociado a {@code key}, o
      *         {@code null} si no existe el registro para {@code key}.
      * @throws NullPointerException si la clave es nula
-     * (<a href="{@docRoot}/java/util/Collection.html#optional-restrictions">optional</a>)
+     *                              (<a href=
+     *                              "{@docRoot}/java/util/Collection.html#optional-restrictions">optional</a>)
      */
-    Integer remove(Object key);
+    V remove(Object key);
 
     /**
      * Elimina todos los registros de la tabla hash
@@ -83,14 +90,17 @@ public interface HashTable{
     void clear();
 
     /**
-     * Retorna el codigo hash para TODA la tabla hash. 
-     * Este codigo hash esta definido por la suma de todos los codigos hash de cada entrada 
-     * de la tabla.  Eso aseguraria que {@code tablaHash1.equals(tablaHash2)} en base a sus hashCode,
-     * es decir, implica que {@code tablaHash1.hashCode() == tablaHash2.hashCode()} para cualquier par de
+     * Retorna el codigo hash para TODA la tabla hash.
+     * Este codigo hash esta definido por la suma de todos los codigos hash de cada
+     * entrada
+     * de la tabla. Eso aseguraria que {@code tablaHash1.equals(tablaHash2)} en base
+     * a sus hashCode,
+     * es decir, implica que {@code tablaHash1.hashCode() == tablaHash2.hashCode()}
+     * para cualquier par de
      * tablas {@code tablaHash1} and {@code tablaHash2},
      */
     int hashCode();
-    
+
     /**
      * Retorna todos los elementos de la tabla hash.
      */
