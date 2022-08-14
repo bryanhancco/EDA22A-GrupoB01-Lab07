46d68f69708bbe3d9d05faab5f8666d2218d89d6
Carlo Rodrigo Diaz Portilla <cdiazpor@unsa.edu.pe>
2022-08-14 16:23:06 -0500

    adding node entry

---
+++ src/Propuestos/Node.java
@@ -0,0 +1,38 @@
+package Propuestos;
+
+public class Node<K, V> {
+  //Atributos
+  private K key;
+  private V value;
+  
+  //Constructor
+  public Node(K key, V value) {
+      this.key = key;
+      this.value = value;
+  }
+  
+  //Setter y Getter
+  public K getKey() {
+      return key;
+  }
+
+  public void setKey(K key) {
+      this.key = key;
+  }
+  public V getValue() {
+      return value;
+  }
+
+  public void setValue(V value) {
+      this.value = value;
+  }
+  public boolean equals(Node<K, V> obj) {
+      return false;
+  }
+
+  @Override
+  public String toString() {
+      return "<" + "key= " + key +", value= " + value + ">";
+  }
+
+}
