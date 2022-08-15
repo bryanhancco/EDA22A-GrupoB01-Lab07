d3844af29810c36d1555b231f76c35cfad66ec56
Gabriel <gmamanican@unsa.edu.pe>
2022-08-14 19:56:04 -0500

    adding getters and setters

--- src/Propuestos/Node.java
+++ src/Propuestos/Node.java
@@ -7,6 +7,23 @@ public class Node<K, V> {
     this.value = value;
   }
 
+  // Setter y Getter de atributos
+  public K getKey() {
+    return key;
+  }
+
+  public void setKey(K key) {
+    this.key = key;
+  }
+
+  public V getValue() {
+    return value;
+  }
+
+  public void setValue(V value) {
+    this.value = value;
+  }
+
   public boolean equals(Node<K, V> obj) {
     return false;
   }
